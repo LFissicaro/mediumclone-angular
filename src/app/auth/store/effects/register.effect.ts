7f51377c990f3cc6cc524b85b9ffccd89ccf69b2
LFissicaro <leofissicaro@gmail.com>
2022-09-01 19:51:59 -0300

    prettier fixes

--- src/app/auth/store/effects/register.effect.ts
+++ src/app/auth/store/effects/register.effect.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { switchMap, of, map, catchError } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { switchMap, of, map, catchError } from "rxjs";
 
-import { AuthService } from 'src/app/auth/services/auth.service';
-import { CurrentUserInterface } from 'src/app/shared/types/currentUser.interface';
+import { AuthService } from "src/app/auth/services/auth.service";
+import { CurrentUserInterface } from "src/app/shared/types/currentUser.interface";
 import {
   registerAction,
   registerFailureAction,
   registerSuccessAction,
-} from 'src/app/auth/store/actions/register.actions';
+} from "src/app/auth/store/actions/register.actions";
 
 @Injectable()
 export class RegisterEffect {
